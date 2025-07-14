b4c68661eb11916848cbbb0d2b25476ac85f2cee
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2025-07-14 16:52:30 -0500

    Added  interface for follower and following

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,18 @@
 Rails.application.routes.draw do
+  # Defines the root path route ("/")
   root "photos#index"
 
-  #get "/users/:id" => "users#show", as: :user
-
-   devise_for :users
-
-  resources :likes
-  resources :follow_requests
+  devise_for :users
   resources :comments
+  resources :follow_requests
+  resources :likes
   resources :photos
 
   get ":username/liked" => "users#liked", as: :liked
+  get ":username/feed" => "users#feed", as: :feed
+  get ":username/followers" => "users#followers", as: :followers
+  get ":username/following" => "users#following", as: :following
 
   get "/:username" => "users#show", as: :user
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  #
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  #get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
