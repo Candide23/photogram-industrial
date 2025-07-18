367c9f23fa42576532c024dd156d8d4a3d0b56bd
Candide Mboungou-Kimpolo <89795037+Candide23@users.noreply.github.com>
2025-07-18 19:37:23 +0000

    Refactor controllers and views; update shared partials and Devise forms

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -59,13 +59,14 @@ class CommentsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_comment
-      @comment = Comment.find(params.expect(:id))
-    end
 
-    # Only allow a list of trusted parameters through.
-    def comment_params
-      params.expect(comment: [ :author_id, :photo_id, :body ])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_comment
+    @comment = Comment.find(params.expect(:id))
+  end
+
+  # Only allow a list of trusted parameters through.
+  def comment_params
+    params.expect(comment: [:author_id, :photo_id, :body])
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,14 +8,10 @@ class UsersController < ApplicationController
   end
 
   def feed
-    @user = User.find_by!(username: params.fetch(:username))
+    @user = current_user
   end
 
-  def followers
-    @user = User.find_by!(username: params.fetch(:username))
-  end
-
-  def following
-    @user = User.find_by!(username: params.fetch(:username))
+  def discover
+    @user = current_user
   end
 end
